81a232903ca923b74da736e1d2acb3ed018f77d2
Pheonvova <pheon1310@gmail.com>
2020-05-14 00:43:51 +0300

    New project
    What's new:
    
    Button - Start
    View - Red

--- Trafficlight(learning)/ViewController.swift
+++ Trafficlight(learning)/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchLightButton: UIButton!
+    
+    var light: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        light = redLight
+        
+        redLight.alpha = 0.3
+        redLight.layer.cornerRadius = 49
+        yellowLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 49
+        greenLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 49
     }
-
-
+    
+    
+    
+    
+    @IBAction func switchLight(_ sender: Any) {
+        
+        
+        
+        switch light {
+        case  redLight:
+            light = yellowLight
+            switchLightButton.setTitle("Red", for: .normal)
+ 
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        case yellowLight:
+            light = greenLight
+            switchLightButton.setTitle("Yellow", for: .normal)
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case greenLight:
+            light = redLight
+            switchLightButton.setTitle("Green", for: .normal)
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        default:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            switchLightButton.setTitle("Off", for: .normal)
+            
+        }
+    }
+    
 }
 
