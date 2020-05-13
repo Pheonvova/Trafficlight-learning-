dfe7bc65ea88efd41e1993cebc91450c1ad80cbe
Pheonvova <pheon1310@gmail.com>
2020-05-13 21:18:25 +0300

    Initial Commit

---
+++ Trafficlight(learning)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trafficlight(learning)
+//
+//  Created by Pheon on 13.05.2020.
+//  Copyright © 2020 Pheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
